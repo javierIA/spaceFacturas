15d693e58f6ed75e01b9993bf1f6b991fef35050
Javier Flores <81644684+javierIA@users.noreply.github.com>
2022-11-23 11:10:18 -0700

    add last metods

--- ia.py
+++ ia.py
@@ -1,6 +1,19 @@
 import os
 import utils
-from tools import PdfTools
+import camelot
+from tools import selectorTemplate
+from helpers.tools import abisa,asbaltos,HUTCHINSON,modine,ssi,table_utils
+import db_custom
+import datetime
+
+def insert_items(Description_items,Quantity_items,Mesure_items,Cost_items):
+    Num_invoice=db_custom.getlastidInvoices()
+    #convert data to float
+    Quantity_items=float(Quantity_items)
+    Cost_items=float(Cost_items)
+    
+    
+    db_custom.insert_item(Description_items,Quantity_items,Mesure_items,Cost_items,Num_invoice)
 def extract_type(path,text):
     rfc={"absia":   "AOM-210617-IC7","absia2":"14660055","asbaltos":"14660055","asbaltos2":"31-1537655","HUTCHINSON":"HSM-000316-H84","modine":"39-048200005","ssi":"SSC-840823-JT3"}
     from utils import RelugarExpretions
@@ -11,24 +24,28 @@ def extract_type(path,text):
             wordlist=list(set(wordlist))
             if len(wordlist)>0:
                 if key=="absia" or key=="absia2":
+                    abisa.getTables(path)
                     return "absia"
                 elif key=="asbaltos" or key=="asbaltos2":
+                    asbaltos.getTables(path)
                     return "asbaltos"
                 elif key=="HUTCHINSON":
+                    HUTCHINSON.getTables(path)
                     return "HUTCHINSON"
                 elif key=="modine":
+                    modine.getTables(path)
                     return "modine"
                 elif key=="ssi":
+                    ssi.getTables(path)
                     return "ssi"
                 else:
+                    table = camelot.read_pdf(path, pages='1-end')
+                    #export to excel 
+                    path=path.replace(".pdf",".xlsx")
+                    for i in range(len(table)):
+                        table[i] = table_utils.clean_table(table[i])
+                        table[i].to_excel(path, sheet_name='number_{}'.format(i))
+                        db_custom.insert_invoice(RFC_clients="IA",Date_invoices=datetime.today(),Total_invoives=0,Origin_invoices=path)
+
                     return "unknown"   
             
-def main():
-    path="C://Users//javie//OneDrive//Documentos//facturas//spaceFacturas//facturas//SISTEMAS Y SERVICIOS DE COMUNICACION SA DE CV"
-    from glob import glob
-    pdfs=glob(path+"//*.pdf")
-    for pdf in pdfs:
-        if PdfTools.ispdfa(pdf):
-            text=PdfTools.reader(pdf)
-            extract_type(pdf,text)  
-main()
